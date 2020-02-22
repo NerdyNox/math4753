a3d690a5f8dff77b1d0b6da4e8742f17c1583145
Nicolas Silva <niksilva28@gmail.com>
2020-02-21 19:13:32 -0600

    Added myncurve

---
+++ R/myncurve.R
@@ -0,0 +1,24 @@
+#' Negative Curve
+#'
+#' @param mu
+#' @param sigma
+#'
+#' @return
+#' @export
+#'
+#' @examples
+myncurve = function(mu, sigma, a){
+  curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu + 3*sigma))
+
+  xcurve=seq(mu-3*sigma,a,length=1000)
+
+  ycurve=dnorm(xcurve,mean=mu,sd=sigma)
+
+  polygon(c(mu-3*sigma,xcurve,a),c(0,ycurve,0),col="Red")
+
+  prob=pnorm(a,mean=mu,sd=sigma)
+
+  prob=round(prob,4)
+
+  paste("Area = ", prob, sep="")
+}
